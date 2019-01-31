1886f6e5ce0daa9b688a178e7854ad7ffd83235c
hoyouly <hoyouly@163.com>
2019-01-31 18:09:52 +0800

    unionfind 并查集

--- src/top/hoyouly/playwithalgorithms/Main.java
+++ src/top/hoyouly/playwithalgorithms/Main.java
@@ -1,10 +1,21 @@
 package top.hoyouly.playwithalgorithms;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
         int i = 8;
         i = ++i;
         System.out.println(i);
+    }
+
+    public static double test() {
+
+        Random random=new Random();
+        long startTime = System.currentTimeMillis();
+
+
+        return (System.currentTimeMillis() - startTime) / 1000.0;
     }
 }
---
+++ src/top/hoyouly/playwithalgorithms/chapter11/Chap11Test.java
@@ -0,0 +1,61 @@
+package top.hoyouly.playwithalgorithms.chapter11;
+
+
+import top.hoyouly.playwithalgorithms.chapter10.Trie;
+import top.hoyouly.playwithalgorithms.chapter7.BSTSet;
+import top.hoyouly.playwithalgorithms.chapter7.FileOperation;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Chap11Test {
+
+    public static void main(String[] args) {
+        int size = 10000000;
+        int num = 10000000;
+        UnionFind1 unionFind1 = new UnionFind1(size);
+        UnionFind2 unionFind2 = new UnionFind2(size);
+        UnionFind3 unionFind3 = new UnionFind3(size);
+        UnionFind4 unionFind4 = new UnionFind4(size);
+        UnionFind5 unionFind5 = new UnionFind5(size);
+        UnionFind6 unionFind6 = new UnionFind6(size);
+
+//        double time = testUF(unionFind1, num);
+//        System.out.println("UnionFind1 time " + time);
+//        double time = testUF(unionFind2, num);
+//        System.out.println("UnionFind2 time " + time);
+
+        double time = testUF(unionFind3, num);
+        System.out.println("UnionFind3 time " + time);
+
+        time = testUF(unionFind4, num);
+        System.out.println("UnionFind3 time " + time);
+        time = testUF(unionFind5, num);
+        System.out.println("unionFind5 time " + time);
+        time = testUF(unionFind6, num);
+        System.out.println("unionFind5 time " + time);
+    }
+
+    private static double testUF(UF uf, int num) {
+        int size = uf.getSize();
+        Random random = new Random();
+        long startTime = System.currentTimeMillis();
+        //先进行合并
+        for (int i = 0; i < num; i++) {
+            int a = random.nextInt(size);
+            int b = random.nextInt(size);
+            uf.unionElements(a, b);
+        }
+
+        //先进行合并
+        for (int i = 0; i < num; i++) {
+            int a = random.nextInt(size);
+            int b = random.nextInt(size);
+            uf.unionElements(a, b);
+        }
+
+        return (System.currentTimeMillis() - startTime) / 1000.0;
+    }
+
+
+}
